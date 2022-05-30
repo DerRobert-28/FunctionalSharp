a79de35be5f679f262d744000f86e8f9115bfa48
Der Robert <eriktainment@gmx.net>
2022-05-30 22:04:06 +0200

    refac: fix mapper and introduce peek

--- FunctionalSharp/Types/Value.cs
+++ FunctionalSharp/Types/Value.cs
@@ -13,31 +13,32 @@
 		public static Value<T> none<T>() => () => default;
 		public static Value<T> some<T>(T value) => () => value;
 
-		public static void map<T>(this Value<T> value, Consumer1<T?> mapper) {
-			if(mapper == null) {
-				throw MappingException.because("Mapper cannot be NULL");
-			} else if(value != null) {
-				mapper.Invoke(value.Invoke());
-			}
-		}
-
 		public static Value<R> map<T, R>(this Value<T> value, Function1<T?, R> mapper) {
 			if(mapper == null) {
 				throw MappingException.because("Mapper cannot be NULL");
 			} else if(value == null) {
 				return none<R>();
-			} else {
-				return of(mapper.Invoke(value.Invoke()));
 			}
+			return of(mapper.Invoke(value.Invoke()));
+		}
+
+		public static Value<T> peek<T>(this Value<T> value, Consumer1<T?> mapper) {
+			if(mapper == null) {
+				throw MappingException.because("Mapper cannot be NULL");
+			} else if(value == null) {
+				return none<T>();
+			}
+			mapper.Invoke(value.Invoke());
+			return value;
 		}
 
 		public static T? get<T>(this Value<T> value) {
 			if(value == null) {
-				throw EvaluationException.because("No value present");
+				throw NoSuchElementException.because("No value present");
 			} else {
 				T? t = value.Invoke();
 				if(t == null) {
-					throw EvaluationException.because("No value present");
+					throw NoSuchElementException.because("No value present");
 				}
 				return t;
 			}
