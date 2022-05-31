b71e090df455db519cce912cee64474ef16ff2ff
Der Robert <eriktainment@gmx.net>
2022-05-31 23:29:41 +0200

    implement NullPointerException

---
+++ FunctionalSharp/Exceptions/NullPointerException.cs
@@ -0,0 +1,9 @@
+﻿namespace DerRobert.FunctionalSharp.Exceptions {
+
+	public class NullPointerException: FunctionalException<NoSuchElementException> {
+
+		private NullPointerException(string reason): base(reason) {}
+
+	}
+
+}
