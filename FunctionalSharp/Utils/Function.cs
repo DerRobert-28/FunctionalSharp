e48cd940db1856848de1e8a130ca04d38847af55
Der Robert <eriktainment@gmx.net>
2022-06-01 22:01:22 +0200

    refix

--- FunctionalSharp/Utils/Function.cs
+++ FunctionalSharp/Utils/Function.cs
@@ -1,8 +1,12 @@
 ﻿namespace DerRobert.FunctionalSharp.Utils {
 
+	using DerRobert.FunctionalSharp.Types.Functions;
+
 
 	public class Function {
 
+		public static Function1<T, T> identity<T>() => t => t;
+		
 		public static T identity<T>(T t) => t;
 
 	}
