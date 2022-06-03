0a001af7248b2d50b6e617a9c8a8651aacc96db2
Der Robert <eriktainment@gmx.net>
2022-06-04 01:48:14 +0200

    fix: correct generic type in class inheritance

--- FunctionalSharp/Exceptions/NullPointerException.cs
+++ FunctionalSharp/Exceptions/NullPointerException.cs
@@ -1,6 +1,6 @@
 ﻿namespace DerRobert.FunctionalSharp.Exceptions {
 
-	public class NullPointerException: FunctionalException<NoSuchElementException> {
+	public class NullPointerException: FunctionalException<NullPointerException> {
 
 		private NullPointerException(string reason): base(reason) {}
 
